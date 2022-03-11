33f8a5a84fb4457a83c6438b7c56757ccaa35bea
BrunoNRS <99756959+BrunoNRS@users.noreply.github.com>
2022-03-11 19:49:44 -0300

    Create csv_extract_module.py

---
+++ csv_extract_module.py
@@ -0,0 +1,44 @@
+import glob,numpy, csv, os
+from osgeo import gdal
+
+data_set_name = "20210106_uva_andorinhas_altum_orto Planta"
+img = "/home/bruno/Projects/Orto Fotos/Projeto Uva/Plant/Output"
+
+def getvalue(file):
+    ds = gdal.Open(file)
+    band = ds.GetRasterBand(1)
+    data = band.ReadAsArray()
+    nodata = band.GetNoDataValue()
+    if nodata is not None:
+        data = numpy.ma.masked_equal(data,nodata,numpy.isnan(data))
+    mean = numpy.nanmean(data)
+    namee = os.path.basename(file)
+    name = os.path.splitext(namee)[0]
+    print(name)
+    var = numpy.nanvar(data)
+    std = numpy.nanstd(data)
+    min = numpy.nanmin(data)
+    max = numpy.nanmax(data)
+    return(name,mean,var,std,min,max)
+
+# # Define inputs folder
+# root = Tkinter.Tk()
+# inputs = tkFileDialog.askdirectory()    
+# root.destroy()
+# print(inputs+" selecionado.")
+
+# if (inputs == None): 
+# 	sair = raw_input('Pasta de entrada nao selecionada.\nPressione enter para sair.\n')
+# 	sys.exit()
+
+inputs = glob.glob(os.path.join(img,"*.tif"))
+
+results = []
+for input in inputs:
+    results.append((input,)+getvalue(input))
+
+output = os.path.join(img, data_set_name + ".csv")# (3) modify output here
+with open(output, 'w') as fp_out:
+    writer = csv.writer(fp_out, delimiter=",", lineterminator='\n')
+    writer.writerow(["filename","name","mean","var","std","min","max"])
+    writer.writerows(results)
