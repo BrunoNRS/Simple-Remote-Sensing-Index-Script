5ba564ef7b43dac0c65edf240a9dacffa19619bd
BrunoNRS <99756959+BrunoNRS@users.noreply.github.com>
2022-06-21 16:17:16 -0300

    Update 1_crop_module.py

--- Altum/1_crop_module.py
+++ Altum/1_crop_module.py
@@ -2,22 +2,18 @@ import glob, os
 import fiona
 import rasterio
 import rasterio.mask
-import numpy as np
 
+#Image folder directory
 img = "D:/PARA_PROCESSAR/Testes/20200903_uva_1030_altum_orto_pixel20cm.tif"
+#Shape files directory 
 shp_path = "D:/PARA_PROCESSAR"
+#Output directory
 out_path = "D:/PARA_PROCESSAR/Testes"
+#Nodata value for border
 no_data = 65535
 
 for shapepath in glob.glob(os.path.join(shp_path, "*.shp")):
-    # try:
-    #     os.mkdir("Crop")
-    # except WindowsError:
-    # Handle the case where the target dir already exist.
-        #pass
-    print(shapepath)
     shapefile =  os.path.basename(shapepath)
-    # print(shapefile)
     shapename = os.path.splitext(shapefile)[0]
     print(shapename)
    
@@ -25,12 +21,12 @@ for shapepath in glob.glob(os.path.join(shp_path, "*.shp")):
     with fiona.open(shapepath, "r") as shapepath:
         shapes = [feature["geometry"] for feature in shapepath]
 
-    # Read imagery file
+    # Read image file
     with rasterio.open(img) as src:
         out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True, nodata = no_data)
         out_meta = src.meta
 
-    # Save clipped imagery
+    # Save clipped image
     out_meta.update({"driver": "GTiff",
                       "height": out_image.shape[1],
                       "width": out_image.shape[2],
