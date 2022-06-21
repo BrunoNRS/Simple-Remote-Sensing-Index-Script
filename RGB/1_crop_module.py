82172af20c8a67a22bf10025fea80be1fb47a154
BrunoNRS <99756959+BrunoNRS@users.noreply.github.com>
2022-06-21 16:31:42 -0300

    Add files via upload

---
+++ RGB/1_crop_module.py
@@ -0,0 +1,36 @@
+import glob, os
+import fiona
+import rasterio
+import rasterio.mask
+
+#Image folder directory
+img = "D:/PARA_PROCESSAR/Testes/20200903_uva_1030_altum_orto_pixel20cm.tif"
+#Shape files directory 
+shp_path = "D:/PARA_PROCESSAR"
+#Output directory
+out_path = "D:/PARA_PROCESSAR/Testes"
+#Nodata value for border
+no_data = 65535
+
+for shapepath in glob.glob(os.path.join(shp_path, "*.shp")):
+    shapefile =  os.path.basename(shapepath)
+    shapename = os.path.splitext(shapefile)[0]
+    print(shapename)
+   
+    # Read Shape file
+    with fiona.open(shapepath, "r") as shapepath:
+        shapes = [feature["geometry"] for feature in shapepath]
+
+    # Read image file
+    with rasterio.open(img) as src:
+        out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True, nodata = no_data)
+        out_meta = src.meta
+
+    # Save clipped image
+    out_meta.update({"driver": "GTiff",
+                      "height": out_image.shape[1],
+                      "width": out_image.shape[2],
+                      "transform": out_transform})
+
+    with rasterio.open(os.path.join(out_path, shapename + '.tif'), "w", **out_meta) as dest:
+        dest.write(out_image)
---
+++ RGB/3_csv_extract_module.py
@@ -0,0 +1,33 @@
+import glob,numpy, csv, os
+from osgeo import gdal
+
+#Input image directory
+img = "/path/to/images"
+
+data_set_name = os.path.basename(img)
+
+def getvalue(file):
+    ds = gdal.Open(file)
+    band = ds.GetRasterBand(1)
+    data = band.ReadAsArray()
+    mean = numpy.nanmean(data)
+    namee = os.path.basename(file)
+    name = os.path.splitext(namee)[0]
+    print(name)
+    var = numpy.nanvar(data)
+    std = numpy.nanstd(data)
+    min = numpy.nanmin(data)
+    max = numpy.nanmax(data)
+    return(name,mean,var,std,min,max)
+
+inputs = glob.glob(os.path.join(img,"*.tif"))
+
+results = []
+for input in inputs:
+    results.append((input,)+getvalue(input))
+
+output = os.path.join(img, data_set_name + ".csv")# (3) modify output here
+with open(output, 'w') as fp_out:
+    writer = csv.writer(fp_out, delimiter=",", lineterminator='\n')
+    writer.writerow(["filename","name","mean","var","std","min","max"])
+    writer.writerows(results)
